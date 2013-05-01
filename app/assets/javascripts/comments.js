1cc75004b1154486f22765d69a452c8e7ebbde56
Kevin Bajorin <KBajorin@gmail.com>
2013-04-30 20:58:04 -0400

    added a toggle to make comment box to dissappear, but they don't work individually

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,9 +4,8 @@
 $(document).ready(function() {
 
 $('body').bind('ajax:success', function(e, data) {
-console.log(data);
-var itemId = $('<div>').append(data).children().first().data("item-id");
-$('.comments-' + itemId).append(data);
-});
-
-});
+	console.log(data);
+	var itemId = $('<div>').append(data).children().first().data("item-id");
+	$('.comments-' + itemId).append(data);
+		});
+	});
